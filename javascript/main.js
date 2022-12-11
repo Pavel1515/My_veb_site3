40c87082a634d7635d1bf6149dfb885ffe8c1817
Frontender <ppvr3407@gmail.com>
2022-12-11 12:29:16 +0200

    +

---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+// Send mail
+
+document.querySelector("#button").addEventListener("click", () => {
+  const name = document.getElementById("name").value;
+  const phone = document.getElementById("phone").value;
+  const templateParams = {
+    name,
+    phone,
+  };
+//   emailjs
+//     .send(
+//       "service_2j214f7",
+//       "template_6ch3ucl",
+//       templateParams,
+//       "aPRv17hWBUrAL6o0w"
+//     )
+//     .then(
+//       function (response) {
+//         console.log("SUCCESS!", response.status, response.text);
+//       },
+//       function (error) {
+//         console.log("FAILED...", error);
+//       }
+//     );
+  alert("Сообщение высланно нам мы вам перезвоним");
+});
